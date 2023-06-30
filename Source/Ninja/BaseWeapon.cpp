9ae8e88941dbc2e1068a81faabb37c2cf5b3fa6d
vidaDaY <127670025+vidaDaY@users.noreply.github.com>
2023-06-30 20:34:32 +0900

    Update BaseWeapon.cpp

--- Source/Ninja/BaseWeapon.cpp
+++ Source/Ninja/BaseWeapon.cpp
@@ -1,23 +1,32 @@
-     // Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "BaseWeapon.h"
 #include "Components/StaticMeshComponent.h"
-#include "Components/SphereComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "Components/SceneComponent.h"
 
 // Sets default values
 ABaseWeapon::ABaseWeapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	RootMesh = CreateDefaultSubobject<USceneComponent>(TEXT("RootMesh"));
-	RootComponent = RootMesh;
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
 	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
-	WeaponMesh->SetupAttachment(RootMesh);
+	WeaponMesh->SetupAttachment(Root);
 
+	collision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("collision"));
+	collision->SetupAttachment(Root);
+	collision->SetGenerateOverlapEvents(false);
+	collision->SetActive(false);
 
+	WeaponTrail = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("WeaponTrail"));
+	WeaponTrail->SetupAttachment(Root);
+	WeaponTrail->SetAutoActivate(false);
 
 }
 
@@ -25,7 +34,7 @@ ABaseWeapon::ABaseWeapon()
 void ABaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -35,5 +44,17 @@ void ABaseWeapon::Tick(float DeltaTime)
 
 }
 
-//Root = CreateDefaultSubobject<USphereComponent>(TEXT("Root"));
-//RootComponent = Root;
+void ABaseWeapon::AttackOn(FName FirstSocketName, FName SecondSocketName)
+{
+	collision->SetActive(true);
+	collision->SetGenerateOverlapEvents(true);
+	WeaponTrail->BeginTrails(FirstSocketName, SecondSocketName, ETrailWidthMode_FromCentre, 1.0f);
+}
+
+void ABaseWeapon::AttackOff()
+{
+	collision->SetActive(false);
+	collision->SetGenerateOverlapEvents(false);
+	WeaponTrail->EndTrails();
+
+}
